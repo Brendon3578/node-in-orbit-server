c9ab2a6887b3b31509382302984d1cc2f322fad1
Brendon Gomes <brendongomes40@gmail.com>
2024-09-10 12:12:42 -0300

    :sparkles: add fastify lib

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import fastify from "fastify";
+
+const app = fastify();
+
+const port = 3333;
+
+app
+  .listen({
+    port: port,
+  })
+  .then(() => {
+    console.log(`HTTP server running on http://localhost:${port}`);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
